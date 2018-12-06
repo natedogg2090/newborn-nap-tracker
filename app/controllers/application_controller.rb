83def530cd3769251a5225580339c51a48fe2610
Nate Dietz <nsdietz@gmail.com>
2018-12-05 20:46:43 -0500

    refactored user related methods to their own controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,44 +15,6 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
-  get "/signup" do
-    if session[:user_id] == nil
-      erb :'users/signup'
-    else
-      redirect to '/index'
-    end
-  end
-
-  post "/signup" do
-    user = User.new(name: params[:name], email: params[:email], password: params[:password])
-    
-    if user.save
-      if user.authenticate(params[:password])
-        session[:user_id] = @user.id
-      end
-      redirect to '/index'
-    else
-      redirect to '/signup'
-    end
-
-  end
-
-  get "/login" do
-    erb :'users/login'
-  end
-
-  post "/login" do
-    user = User.find_by(email: params[:email])
-
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect to '/index'
-    else
-      redirect to '/signup'
-    end
-    
-  end
-
   get "/index" do
     if logged_in?
       @user = User.find_by(session[:user_id])
@@ -154,15 +116,6 @@ class ApplicationController < Sinatra::Base
       flash[:message] = "This nap has been updated. Are you getting some rest?"
 
       redirect to "naps/#{naps.id}"
-    end
-  end
-
-  get "/logut" do
-    if session[:user_id] != nil
-      session.clear
-      redirect to "/"
-    else
-      redirect to "/"
     end
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,50 @@
 class UsersController < ApplicationController
 
+  get "/signup" do
+    if session[:user_id] == nil
+      erb :'users/signup'
+    else
+      redirect to '/index'
+    end
+  end
+
+  post "/signup" do
+    user = User.new(name: params[:name], email: params[:email], password: params[:password])
+    
+    if user.save
+      if user.authenticate(params[:password])
+        session[:user_id] = @user.id
+      end
+      redirect to '/index'
+    else
+      redirect to '/signup'
+    end
+
+  end
+
+  get "/login" do
+    erb :'users/login'
+  end
+
+  post "/login" do
+    user = User.find_by(email: params[:email])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to '/index'
+    else
+      redirect to '/signup'
+    end
+    
+  end
+
+  get "/logut" do
+    if session[:user_id] != nil
+      session.clear
+      redirect to "/"
+    else
+      redirect to "/"
+    end
+  end
+
 end
