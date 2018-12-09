528affc79441f878a9f2320268a7708bbc054fa5
Nate Dietz <nsdietz@gmail.com>
2018-12-08 21:55:33 -0500

    added validations for baby model

--- app/models/baby.rb
+++ app/models/baby.rb
@@ -2,4 +2,8 @@ class Baby < ActiveRecord::Base
   belongs_to :user
   has_many :naps
 
+  validates :name, :presence => true
+  validates :birthday, :presence => true
+  validates :user_id, :presence => true
+
 end
