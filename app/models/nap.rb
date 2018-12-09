480d4aeeed9fccf1a798db6a9c22005fd03124df
Nate Dietz <nsdietz@gmail.com>
2018-12-08 21:54:25 -0500

    added validations for naps

--- app/models/nap.rb
+++ app/models/nap.rb
@@ -1,4 +1,9 @@
 class Nap < ActiveRecord::Base
   belongs_to :baby
 
+  validates :start_time, :presence => true
+  validates :end_time, :presence => true
+  validates :notes, :presence => true
+  validates :baby_id, :presence => true
+
 end
